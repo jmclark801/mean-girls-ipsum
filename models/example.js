20dbeed0ced3c23a3f08a13960e605a4f4e139cd
Diana Carrara <dianaVcarrara@gmail.com>
2019-06-27 20:28:34 -0600

    added seq model again, changed rule

--- models/example.js
+++ models/example.js
@@ -2,10 +2,10 @@ module.exports = function(sequelize, DataTypes) {
   var Quotes = sequelize.define("Quotes", {
     quote: {
       type: DataTypes.STRING(800),
-      allowNull: false,
-      },
-      character_name: DataTypes.STRING,
-      voice_over: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    character_name: DataTypes.STRING,
+    voice_over: DataTypes.BOOLEAN
   });
 
   return Quotes;
